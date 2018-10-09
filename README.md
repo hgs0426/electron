890e6fff27b94d8646a027ec814a9790c82971df
hwang <hgs0426@gmail.com>
2018-10-09 23:55:00 +0900

    hello_world pjts

---
+++ README.md
@@ -0,0 +1,1 @@
+Electron for simple desktop app
---
+++ hello_world/main.js
@@ -0,0 +1,13 @@
+/* main.js */
+
+const {app, BrowserWindow} = require('electron');
+
+let win  
+
+function createWindow() { 
+   win = new BrowserWindow({width: 800, height: 600});
+   /* load local .html file*/
+   win.loadURL(`file://${__dirname}/index.html`);
+}  
+
+app.on('ready', createWindow); 
